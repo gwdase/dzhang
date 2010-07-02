4137284a0cea146a187320870e4ed2082b17e3cd
dzhang <gwdase@gmail.com>
2010-07-02 10:42:24 +0800

    kohana

--- application/logs/2010/07/01.php
+++ application/logs/2010/07/01.php
@@ -4,3 +4,4 @@
 2010-07-01 21:23:15 --- ERROR: ReflectionException [ -1 ]: Class controller_dzhang does not exist ~ SYSPATH/classes\kohana\request.php [ 1007 ]
 2010-07-01 21:23:16 --- ERROR: ReflectionException [ -1 ]: Class controller_dzhang does not exist ~ SYSPATH/classes\kohana\request.php [ 1007 ]
 2010-07-01 21:23:17 --- ERROR: ReflectionException [ -1 ]: Class controller_dzhang does not exist ~ SYSPATH/classes\kohana\request.php [ 1007 ]
+2010-07-01 21:38:48 --- ERROR: ErrorException [ 1 ]: Call to undefined function site_url() ~ APPPATH/views\layout.php [ 10 ]
