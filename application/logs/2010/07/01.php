b95f6604f0531ddcd56c291efcf75cc8ebac77e3
dzhang <gwdase@gmail.com>
2010-07-02 10:49:44 +0800

    kohana

--- application/logs/2010/07/01.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-07-01 21:22:19 --- ERROR: ReflectionException [ -1 ]: Class controller_phpcode does not exist ~ SYSPATH/classes\kohana\request.php [ 1007 ]
-2010-07-01 21:23:15 --- ERROR: ReflectionException [ -1 ]: Class controller_dzhang does not exist ~ SYSPATH/classes\kohana\request.php [ 1007 ]
-2010-07-01 21:23:16 --- ERROR: ReflectionException [ -1 ]: Class controller_dzhang does not exist ~ SYSPATH/classes\kohana\request.php [ 1007 ]
-2010-07-01 21:23:17 --- ERROR: ReflectionException [ -1 ]: Class controller_dzhang does not exist ~ SYSPATH/classes\kohana\request.php [ 1007 ]
-2010-07-01 21:38:48 --- ERROR: ErrorException [ 1 ]: Call to undefined function site_url() ~ APPPATH/views\layout.php [ 10 ]
